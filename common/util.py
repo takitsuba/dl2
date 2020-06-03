b89aad2a4176298588f7978f87b6f5ae5f40872d
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-03 23:21:23 +0900

    コサイン類似度上位を表示する関数作成

--- common/util.py
+++ common/util.py
@@ -49,3 +49,32 @@ def cos_similarity(x, y, eps=1e-8):
     nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
     ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
     return np.dot(nx, ny)
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top):
+    # queryがそもそもなければ何もできない
+    if query not in word_to_id:
+        print("%s is not found" % query)
+        return
+
+    print("\n[query]", query)
+    # queryのベクトルを取り出す
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    # queryのベクトルとその他のベクトルのコサイン類似度を
+    vocab_size = len(word_to_id)
+    similarities = np.zeros(vocab_size)
+
+    for i in range(vocab_size):
+        similarities[i] = cos_similarity(word_matrix[i], query_vec)
+
+    count = 0
+    # -1をかけているのはsimilaritiesが大きい順にしたいため
+    for i in (-1 * similarities).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(" %s: %s" % (id_to_word[i], similarities[i]))
+        count += 1
+        if count >= top:
+            return
