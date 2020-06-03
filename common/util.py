00b9718bcd469bee102ac299675276e9c226de43
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-03 21:09:48 +0900

    add some comment

--- common/util.py
+++ common/util.py
@@ -15,6 +15,8 @@ def preprocess(text):
             word_to_id[word] = new_id
             id_to_word[new_id] = word
 
+    # 上記のfor文の中で同時に作成していくことも可能で
+    # そちらの方が計算回数は減りそうだが、見通しが悪そう
     corpus = np.array([word_to_id[w] for w in words])
 
     return corpus, word_to_id, id_to_word
