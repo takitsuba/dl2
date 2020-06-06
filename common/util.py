1d69701896665beffdd20b812df01f0076ce1556
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-06 22:02:17 +0900

    one-hotベクトルに変換する関数

--- common/util.py
+++ common/util.py
@@ -114,3 +114,30 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
 
     return np.array(contexts), np.array(target)
+
+
+def convert_one_hot(corpus, vocab_size):
+    """one-hot表現への変換
+
+    :param corpus: 単語IDのリスト（1次元もしくは2次元のnumpy配列)
+    :param vocab_size: 語彙数
+    :return: one-hot表現（2次元もしくは3次元のnumpy配列）
+    """
+
+    N = corpus.shape[0]
+
+    # targetの場合はcorpusが1次元
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    # contextsの場合はcorpusが2次元
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, words in enumerate(corpus):
+            for idx_1, word_id in words:
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
