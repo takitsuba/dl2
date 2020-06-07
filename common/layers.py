484d461eda7e0a128bd35c1641d160ec38dd2b3d
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-07 19:15:44 +0900

    Embeddingにbackward追加

--- common/layers.py
+++ common/layers.py
@@ -117,3 +117,10 @@ class Embedding:
         self.idx = idx
         out = W[idx]
         return out
+
+    def backward(self, dout):
+        (dW,) = self.grads
+        dW[...] = 0
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+        return None
