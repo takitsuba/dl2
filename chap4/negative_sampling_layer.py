0f8e6bb60cf129237367f121cae1aa9835caf856
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-07 23:17:37 +0900

    EmbeddingDotレイヤの作成

---
+++ chap4/negative_sampling_layer.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.append("..")
+from common.layers import Embedding
+import numpy as np
+
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target = self.cache
+
+        # doutはベクトルなので、shapeを行列にしているということかな
+        dout = dout.reshape(dout.shape[0], 1)
+
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * dtarget_W
+        return dh
