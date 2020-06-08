60f53505344ffccaa128942329e51aa66eed07b1
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-08 21:54:07 +0900

    sigmoid with lossを追加

--- common/layers.py
+++ common/layers.py
@@ -86,6 +86,29 @@ class Sigmoid:
         return dx
 
 
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params = []
+        self.grads = []
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        # 1-y, yのところがよくわからない。
+        loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        # batch_sizeで割ってるのはなんでだっけ
+        dx = dout * (self.y - self.t) / batch_size
+        return dx
+
+
 class MatMul:
     def __init__(self, W):
         self.params = [W]
