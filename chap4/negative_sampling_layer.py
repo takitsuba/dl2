785dc1efdbee1f6e870e63baf4f4501b3e4239fd
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-08 13:45:48 +0900

    UnigramSamplerのinitを作成(書籍と異なる)

--- chap4/negative_sampling_layer.py
+++ chap4/negative_sampling_layer.py
@@ -3,6 +3,7 @@ import sys
 sys.append("..")
 from common.layers import Embedding
 import numpy as np
+from collections import Counter
 
 
 class EmbeddingDot:
@@ -29,3 +30,25 @@ class EmbeddingDot:
         self.embed.backward(dtarget_W)
         dh = dout * dtarget_W
         return dh
+
+
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        # Counterに突っ込めば各要素の出現回数が辞書で得られるはず
+        counts = Counter(corpus)
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        corpus_size = len(corpus)
+
+        for i in range(vocab_size):
+            # ある単語の確率は、ある単語の出現回数を全単語の出現回数で割り求める
+            self.word_p[i] = counts[i] / corpus_size
+
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
