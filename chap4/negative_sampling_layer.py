883850b941c41df3015e93a1e8d67178d626f542
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-08 22:00:52 +0900

    negative samplingし正例負例合わせたlossを求める

--- chap4/negative_sampling_layer.py
+++ chap4/negative_sampling_layer.py
@@ -1,7 +1,7 @@
 import sys
 
 sys.append("..")
-from common.layers import Embedding
+from common.layers import Embedding, SigmoidWithLoss
 import numpy as np
 from collections import Counter
 
@@ -67,3 +67,46 @@ class UnigramSampler:
             )
 
         return negative_sample
+
+
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        # 正例1と負例sample_sizeのためsample_size+1
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+
+        self.params, self.grads = [], []
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        # UnigramSamplerのget_negative_sampleをもとに負例のサンプリングを行う
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        # 正例のforward
+        # embed_dot_layers と loss_layers の 0番目が正例のlayerであるとする
+        score = self.embed_dot_layers[0].forward(h, target)
+        # 正例に対する正解ラベルは1。
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        # 負例のforward
+        # 負例に対する正解ラベルは0。
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1 + i].forward(h, negative_target)
+            loss += self.loss_layers[1 + i].forward(score, negative_label)
+
+        return loss
+
+    def backward(self, dout=1):
+        dh = 0
+        for l0, l1 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+        return dh
