c6d2cb62264ccf40d8e3417cb8ca9b4a9191bf9c
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-08 14:29:59 +0900

    negative samplingから得たidxを取得するメソッド

--- chap4/negative_sampling_layer.py
+++ chap4/negative_sampling_layer.py
@@ -52,3 +52,18 @@ class UnigramSampler:
 
         self.word_p = np.power(self.word_p, power)
         self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+        negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+        for i in range(batch_size):
+            p = self.word_p.copy()
+            target_idx = target[i]
+            p[target_idx] = 0
+            p /= p.sum()
+            negative_sample[i, :] = np.random.choice(
+                self.vocab_size, size=self.sample_size, p=p
+            )
+
+        return negative_sample
