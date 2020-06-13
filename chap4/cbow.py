8a25c65ff485469171be7bf16e93eb011bf443c4
Hiroo Takizawa <kei.cataract@gmail.com>
2020-06-13 17:27:56 +0900

    CBOWクラスの作成

---
+++ chap4/cbow.py
@@ -0,0 +1,50 @@
+import sys
+
+sys.path.append("..")
+import numpy as np
+from common.layers import Embedding
+from negative_sampling_layer import NegativeSamplingLoss
+
+
+class CBOW:
+    def __init__(self, vocab_size, hidden_size, window_size, corpus):
+        V, H = vocab_size, hidden_size
+
+        W_in = 0.01 * np.random.randn(V, H).astype("f")
+        W_out = 0.01 * np.random.randn(V, H).astype("f")
+
+        # レイヤの生成
+        self.in_layers = []
+        for i in range(2 * window_size):
+            layer = Embedding(W_in)
+            self.in_layers.append(layer)
+        # 負例は5つサンプリングするように設定
+        self.ns_loss = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
+
+        layers = self.in_layers + [self.ns_loss]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # メンバ変数に単語の分散表現を設定
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        # 中間層の計算
+        h = 0
+        for i, layer in self.in_layers:
+            h += layer.forward(contexts[:, i])
+        h *= 1 / len(self.in_layers)
+
+        # lossの計算
+        loss = self.ns_loss.forward(h, target)
+        return loss
+
+    def backward(self, dout=1):
+        dh = self.ns_loss.backward(dout)
+        dh *= 1 / len(self.in_layers)
+        for layer in self.in_layers:
+            layer.backward(dh)
+
+        return None
